ac3d23211cdd28f6157a5c6bf2b2d6639179149b
iamaian <The21stKuruno@yahoo.com>
2018-03-14 23:56:07 +0900

    form style

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+      var typed = new Typed('.element', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 110,
+        backDelay: 1000,
+        loop: true,
+        loopCount: Infinity,
+      });
+  });
