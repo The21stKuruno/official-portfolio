e886bfe42b747dbeaf1dffd604a17761be4a7329
iamaian <The21stKuruno@yahoo.com>
2018-03-15 23:51:14 +0900

    just started javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-$(function(){
-      var typed = new Typed('.element', {
-        stringsElement: '#typed-strings',
-        typeSpeed: 110,
-        backDelay: 1000,
-        loop: true,
-        loopCount: Infinity,
-      });
+$(function() {
+  // type action
+  var typed = new Typed('.element', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 110,
+    backDelay: 1000,
+    loop: true,
+    loopCount: Infinity,
   });
+  // scroll to latest-work id
+  setBindings();
+});
+
+function setBindings() {
+  $("nav a").click(function(e) {
+    alert('button id ' + e.currentTarget)
+  });
+}
+// https://www.youtube.com/watch?v=gZudXREGviM
